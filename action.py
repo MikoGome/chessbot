31010a0299166c4607bdcb3f582c3204391f8e41
Live session user <lunalescia@gmail.com>
2025-03-22 21:42:01 -0700

    pdated

--- action.py
+++ action.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import keyboard
 
 def move(move, board):
     begin = move[0:2]
@@ -7,16 +8,17 @@ def move(move, board):
     init = convert(begin, board)
     final = convert(end, board)
     padding = 30
+    keyboard.wait('q')
     subprocess.call(["xdotool", "mousemove", str(init[0]-padding), str(init[1]+padding)])
     subprocess.call(["xdotool", "click", "1"])
-    time.sleep(1)
+    time.sleep(0.1)
     subprocess.call(["xdotool", "mousemove", str(final[0]-padding), str(final[1]+padding)])
     subprocess.call(["xdotool", "click", "1"])
-    subprocess.call(["xdotool", "mousemove", "100", "10"])
+    subprocess.call(["xdotool", "mousemove", "500", "10"])
     if len(move) > 4:
         subprocess.call(["xdotool", "mousemove", str(final[0]-padding), str(final[1]+padding)])
         subprocess.call(["xdotool", "click", "1"])
-        subprocess.call(["xdotool", "mousemove", "100", "10"])
+        subprocess.call(["xdotool", "mousemove", "500", "10"])
 
 def convert(coord, board):
     for square in board:
--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from chesscomboard import initialize, flip
 from action import move
 
 def main():
-    engine = chess.engine.SimpleEngine.popen_uci("./stockfish/stockfish-ubuntu-x86-64-avx2");
+    engine = chess.engine.SimpleEngine.popen_uci("./stockfish/stockfish-ubuntu-x86-64-avx2")
     
+    engine.configure({"Skill Level": 6})
     board = chess.Board()
     vizboard = initialize()
 
@@ -46,22 +47,21 @@ def opponent_move(board, chess, vizboard, player):
     while True:
         if board.is_game_over():
             break
-        time.sleep(1)
+            
         inputArr = scan(vizboard)
         if len(inputArr) < 2:
             continue
+
         opponent_move = inputArr[0]+inputArr[1]
         if chess.Move.from_uci(opponent_move) not in board.legal_moves:
             opponent_move = inputArr[1]+inputArr[0]
         if chess.Move.from_uci(opponent_move) not in board.legal_moves:
-            if player == 'w':
-                opponent_move = inputArr[0]+inputArr[1] + 'q'
-            elif player == 'q':
-                opponent_move = inputArr[0]+inputArr[1] + 'Q'
+            opponent_move = inputArr[0]+inputArr[1] + 'q'
         if chess.Move.from_uci(opponent_move) in board.legal_moves:
             move = chess.Move.from_uci(opponent_move)
             board.push(move)
             break
+        time.sleep(1)
     
 if __name__ == "__main__":
     main()
